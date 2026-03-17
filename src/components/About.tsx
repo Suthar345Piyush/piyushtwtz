1bb5826f97937d425cd1e033af89764d63cca868
Suthar345Piyush <piyushsuthar524@gmail.com>
2026-03-17 21:38:35 +0530

    made some changes

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -12,18 +12,29 @@ export default function About() {
 
         <motion.section initial={{opacity : 0 , y : 20}} animate={{opacity : 1 , y : 0}} transition={{duration : 0.5 , delay : 0.1}} className='mb-16'>
 
-          <h2 className='text-3xl font-bold mb-6 text-white'>
+          <h2 className='text-3xl font-bold mb-2 text-white'>
             about me/.
           </h2>
 
           <div className='space-y-4 text-white leading-relaxed'>
-             <p>learnt by doing things and by implementing , love to build web & mobile apps</p>
-
-             <p>i like general tech , space tech , physics and science. they make everything possible in this universe</p>
-
-             <p>i write code for web & mobile apps</p>
+            <p>
+              a 20 year's old full stack developer from india, love to build real products which make impact on market, i mostly spend my time on building and learning software technologies, love to build, ship and maintaining products on the internet.
+              </p>
+          
 
            </div>
+
+              <div className=''>
+
+               <div>
+
+               </div>
+
+           </div>
+
+
+
+           
 
         </motion.section>
 
